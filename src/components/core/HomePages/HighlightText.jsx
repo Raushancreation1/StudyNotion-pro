5bacc7c92bb438e69e7a8e983c5e5f0b8ab128e1
Raushan <raushancreationofficial@.com>
2025-08-14 20:31:46 +0530

    new code update

---
+++ src/components/core/HomePages/HighlightText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HighlightText = ({text}) => {
+  return (
+    <sapn className='font-bold text-richblue-200'>
+        {" "}
+        {text}
+    </sapn>
+  )
+}
+
+export default HighlightText
---
+++ src/pages/Home.jsx
@@ -0,0 +1,143 @@
+import React from 'react'
+import { FaArrowRight } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import HighlightText from '../components/core/HomePages/HighlightText';
+import CTAButton from '../components/core/HomePages/Button';
+import Banner from "../assets/Images/banner.mp4";
+import CodeBlocks from '../components/core/HomePages/CodeBlocks';
+
+const Home = () => {
+  return (
+    <div>
+            {/* Section1 */}
+            <div className='relative mx-auto flex flex-col w-11/12 items-center text-white justify-between '>
+
+                
+                    <Link to={"/singup"}>
+
+                        <div className='group mt-16 p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200 transition-all duration-200 hover:sacle-95 w-fit'>
+
+                            <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900 '>
+                                <p>Become an Instructor</p>
+                                 <FaArrowRight />
+                            </div>
+                        </div>
+
+                    </Link>
+
+                    <div className='text-center tex-4xl font-semibold mt-7 '>
+                        Empower Your Future with
+                        <HighlightText text={"Coding Skills"}/> 
+                    </div>
+
+                    <div className='mt-4 w-[90%] text-center text-lg font-bold text-richblack-300'>
+                        With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors. 
+                    </div>
+
+                    <div className='flex flex-row gap-7 mt-8'>
+
+                        <CTAButton active={true} linkto={"/signup"}>
+                            Learn More
+                        </CTAButton>
+
+                        <CTAButton active={false} linkto={"/login"}>
+                            Book a Demo
+                        </CTAButton>
+
+                    </div>
+
+                    <div className='mx-5 mt-12 shadow-blue-200'>
+
+                        <video
+                        muted
+                        loop
+                        autoPlay
+                        >
+
+                            <source src={Banner} type="video/mp4" />
+                        </video> 
+
+                    </div>
+
+                    {/*Code section1 */}
+                    <div>
+                        <CodeBlocks
+                             position={"lg:flex-row"}
+                                heading={
+                                    <div className='text-4xl font-semibold'>
+                                        Unlock Your
+                                        <HighlightText text={"Coding Potential"}/>
+                                        with our online courses
+                                    </div>
+
+                                }
+                                subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."}
+
+                            ctabtn1={
+                                {
+                                    btnText:"Try it Yourself",
+                                    linkto:"/signup",
+                                    active:true,
+                                }
+                            }
+                             
+                            ctabtn2={
+                                {
+                                    btnText:"learn More",
+                                    linkto:"/login",
+                                    active:false,
+                                }
+                            }
+
+                            codeblocks={`<<!DOCTYPE html>\n<html>\n<head><title>Example</title><linkrel="stylesheet"href="styles.css">\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>>`}
+                            
+                        />
+                    </div>
+
+
+                    {/*Code section2 */}
+                    <div>
+                        <CodeBlocks
+                             position={"lg:flex-row"}
+                                heading={
+                                    <div className='text-4xl font-semibold'>
+                                        Unlock Your
+                                        <HighlightText text={"Coding Potential"}/>
+                                        with our online courses
+                                    </div>
+
+                                }
+                                subheading={"Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."}
+
+                            ctabtn1={
+                                {
+                                    btnText:"Try it Yourself",
+                                    linkto:"/signup",
+                                    active:true,
+                                }
+                            }
+                             
+                            ctabtn2={
+                                {
+                                    btnText:"learn More",
+                                    linkto:"/login",
+                                    active:false,
+                                }
+                            }
+
+                            codeblocks={`<<!DOCTYPE html>\n<html>\n<head><title>Example</title><linkrel="stylesheet"href="styles.css">\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>>`}
+                            
+                        />
+                    </div>
+            </div>
+
+        {/* Section1 */}
+
+        {/* Section1 */}
+
+        {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
